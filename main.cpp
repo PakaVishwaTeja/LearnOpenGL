fa30e82227defaab1fad173e62204ee4de870e34
PakaVishwaTeja <67907453+PakaVishwaTeja@users.noreply.github.com>
2024-03-02 20:57:37 +0530

    4 . Shaders and more (#2)
    * send data from shader to shader
    
    * using uniforms to send data to shaders
    
    * added more vertex attributes
    
    * 4. Shaders and more

--- main.cpp
+++ main.cpp
@@ -3,21 +3,19 @@
 #include <GLFW/glfw3.h>
 #include <string>
 #include<fstream>
+#include<shader.h>
 using namespace std;
 
 GLFWwindow* window;
 GLuint VBO;
 GLuint VAO;
 GLuint EBO;
-GLuint gGraphicsPipeline;
+shader* shaderProgram;
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
+
 void initialize();
 void vertexSpecification();
-void createGraphicsPipeline();
-    string loadShaderAsString( const string &filename);
-        GLuint createShaderProgram(string vs , string fs);
-            GLuint compileShader(GLuint type ,string src);
 void mainloop();
     void predraw();
     void draw();
@@ -27,7 +25,10 @@ int main(){
     cout<<"hello humans!"<<endl;
     initialize();
     vertexSpecification();
-    createGraphicsPipeline();
+    shaderProgram = new shader(
+        "/Users/vishwa/dev/Learn_OpenGL/shaders/vert.glsl",
+        "/Users/vishwa/dev/Learn_OpenGL/shaders/frag.glsl"
+        );
     mainloop();
     cleanup();
 
@@ -68,14 +69,14 @@ void initialize(){
 
 void vertexSpecification(){
     GLfloat vertices[] = {
-        0.5f,  0.5f, 0.0f,  // top right
-        0.5f, -0.5f, 0.0f,  // bottom right
-        -0.5f, -0.5f, 0.0f,  // bottom left
-        -0.5f,  0.5f, 0.0f   // top left 
-    };
+    // positions         // colors
+     0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
+    -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
+     0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top 
+}; 
     unsigned int indices[] = {
-        0, 1, 3,   // first triangle
-        1, 2, 3    // second triangle
+        0, 1, 2   // first triangle
+        //1, 2, 3    // second triangle
     };
 
     glGenVertexArrays(1, &VAO);
@@ -92,84 +93,17 @@ void vertexSpecification(){
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW); 
 
-    glVertexAttribPointer(0,3,GL_FLOAT , GL_FALSE , 3 * sizeof(GLfloat) , (void*)0);
+    //vertex position attribute
+    glVertexAttribPointer(0,3,GL_FLOAT , GL_FALSE , 6 * sizeof(GLfloat) , (void*)0);
     glEnableVertexAttribArray(0);
+    //vertex color attribute
+    glVertexAttribPointer(1,3,GL_FLOAT , GL_FALSE , 6 * sizeof(GLfloat) , (void*)(3*sizeof(float)));
+    glEnableVertexAttribArray(1);
 
     //unbind
     glBindBuffer(GL_ARRAY_BUFFER , 0);
     glBindVertexArray(0); 
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER , 0);//unbind after unbinding VAO
-}
-void createGraphicsPipeline(){
-    //load shaders
-    string vsSrc  = loadShaderAsString("/Users/vishwa/dev/Learn_OpenGL/shaders/vert.glsl");
-    string fsSrc  = loadShaderAsString("/Users/vishwa/dev/Learn_OpenGL/shaders/frag.glsl");
-
-    gGraphicsPipeline = createShaderProgram(vsSrc , fsSrc);
-}
-GLuint createShaderProgram(string vs , string fs){
-    GLuint programObject  = glCreateProgram();
-
-    //compiling shader source
-    GLuint vertexShader = compileShader(GL_VERTEX_SHADER,vs);
-    GLuint fragmentShader = compileShader(GL_FRAGMENT_SHADER,fs);
-
-    //linking shaders to program object i.e graphics pipeline
-    glAttachShader(programObject , vertexShader);
-    glAttachShader(programObject , fragmentShader);
-    glLinkProgram(programObject);
-    glValidateProgram(programObject);
-        // Check for shader program linking errors
-            GLint success;
-
-    glGetProgramiv(programObject, GL_LINK_STATUS, &success);
-    if (!success) {
-        char infoLog[512];
-        glGetProgramInfoLog(programObject, 512, NULL, infoLog);
-        std::cerr << "Shader program linking failed:\n" << infoLog << std::endl;
-        return -1;
-    }
-
-    return programObject;
-}
-GLuint compileShader(GLuint type ,string src){
-    GLuint shaderObject;
-    shaderObject = glCreateShader(type);
-
-    const char* source = src.c_str();
-    GLint codelength[1];
-    codelength[0] = strlen(source);
-
-    glShaderSource(shaderObject , 1 , &source ,codelength );
-    glCompileShader(shaderObject);
-
-    // Check for vertex shader compilation errors
-    GLint success;
-    glGetShaderiv(shaderObject, GL_COMPILE_STATUS, &success);
-    if (!success) {
-        char infoLog[512];
-        glGetShaderInfoLog(shaderObject, 512, NULL, infoLog);
-        std::cerr << "Vertex shader compilation failed:\n" << infoLog << std::endl;
-        return -1;
-    }
-
-    return shaderObject;
-}
-string loadShaderAsString( const string &filename){
-    string result = "";
-    string line = "";
-    ifstream myFile;
-    myFile.open(filename);
-    if(myFile.is_open()){
-        while(getline(myFile , line)){
-            result += line + '\n';
-        }
-        myFile.close();
-    }else{
-        std::cerr << "Error opening file: " << strerror(errno) << std::endl;
-        cout<<filename<<" not opened!"<<endl;
-    }
-    return result;
 }
 
 void mainloop(){
@@ -184,25 +118,37 @@ void mainloop(){
     }
 }
 void predraw(){
+
+    float timeValue = glfwGetTime();
+    // float gValue = (sin(timeValue) / 2.0f) + 0.5f;
+    // float rValue = (sin(timeValue + 3) / 2.0f) + 0.5f;
+    // float bValue = (sin(timeValue + 5) / 2.0f) + 0.5f;
+
         //  glClearColor function is a _state-setting_ function
-        glClearColor(0.0f, 0.5f, 0.3f, 1.0f);
-        
+        //glClearColor(rValue, gValue,bValue, 1.0f);
+        glClearColor(0.2, 0.3,0.2, 1.0f);
         //glClear is a _state-using_ function. 
         //It uses the current state to retrieve the clearing color from.
         glClear(GL_COLOR_BUFFER_BIT);
 }
 void draw(){
-  glUseProgram(gGraphicsPipeline);
+
+  shaderProgram->use();
+
+  //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+  shaderProgram->setFloat4( "ourColor" ,  0.2,0.2,0.2,1.0);
+
   glBindVertexArray(VAO);
-//   glDrawArrays(GL_TRIANGLES , 0 , 3);
-  glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+  //glDrawArrays(GL_TRIANGLES , 0 , 3);
+  glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
 }
 void cleanup(){
     
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
     glDeleteBuffers(1, &EBO);
-    glDeleteProgram(gGraphicsPipeline);
+    
     //clean glfw
     glfwTerminate();
 }
--- shaders/frag.glsl
+++ shaders/frag.glsl
@@ -1,5 +1,9 @@
 #version 330 core
+in vec3 vertexColor;
+//uniform vec4 ourColor;
+
 out vec4 FragColor;
 void main(){
- FragColor = vec4(1.0f, 0.0f, 0.0f, 1.0f);
+ //FragColor = vec4(0.4f, 0.0f, 0.0f, 1.0f);
+ FragColor =vec4(vertexColor,1.0);
 }
