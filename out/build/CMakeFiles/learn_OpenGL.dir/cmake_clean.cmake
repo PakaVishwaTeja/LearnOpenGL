3f46cff1198137444e499070cc932a822e79ce6f
vishwa <tejapaka08@gmail.com>
2024-03-21 10:23:12 +0530

    all

---
+++ out/build/CMakeFiles/learn_OpenGL.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/learn_OpenGL.dir/main.cpp.o"
+  "CMakeFiles/learn_OpenGL.dir/main.cpp.o.d"
+  "learn_OpenGL"
+  "learn_OpenGL.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/learn_OpenGL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ out/build/CMakeFiles/learn_OpenGL.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for learn_OpenGL.
+# This may be replaced when dependencies are built.
